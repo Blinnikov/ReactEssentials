0a86ae3363e628c30b26931c7e884f1a3540837f
Igor Blinnikov <igor.blinnikov@gmail.com>
2016-07-12 22:57:51 +0300

    feat(notes): stylings
    feat(notes): addition, saving

--- Lesson-02/06-notes/notes.js
+++ Lesson-02/06-notes/notes.js
@@ -8,9 +8,25 @@ var Note = React.createClass({
 });
 
 var NotesGrid = React.createClass({
+  componentDidMount: function() {
+    var grid = this.refs.grid;
+    this.msnry = new Masonry(grid, {
+      // options
+      itemSelector: '.note',
+      columnWidth: 200,
+      gutter: 10,
+      isFitWidth: true
+    });
+  },
+  componentDidUpdate: function(prevProps) {
+    if(this.props.notes.length != prevProps.notes.length) {
+        this.msnry.reloadItems();
+        this.msnry.layout();
+    }
+  },
   render: function() {
     return (
-      <div className="notes-grid">
+      <div className="notes-grid" ref="grid">
         {
           this.props.notes.map(function(note) {
             return <Note key={note.id} color={note.color} >
@@ -24,9 +40,39 @@ var NotesGrid = React.createClass({
 });
 
 var NoteEditor = React.createClass({
+  getInitialState: function() {
+    return {
+      text: ''
+    }
+  },
+  onTextChange: function(event) {
+    this.setState({
+      text: event.target.value
+    });
+  },
+  addNote: function() {
+    var note = {
+      id: Date.now(),
+      text: this.state.text,
+      color: 'yellow'
+    };
+    this.props.onNoteAdd(note);
+    this.setState({
+      text: ''
+    });
+  },
   render: function() {
     return (
-        <div className="note-editor"> Editor </div>
+        <div className="note-editor">
+          <textarea
+            placeholder="Enter yout text here.."
+            rows={5}
+            className="textarea"
+            value={this.state.text}
+            onChange={this.onTextChange}
+          />
+        <button className="add-button" onClick={this.addNote}>Add</button>
+        </div>
     );
   }
 });
@@ -35,54 +81,76 @@ var App = React.createClass({
   getInitialState: function() {
     return {
       notes: [
-        {
-          id: 0,
-          text: 'Text 01',
-          color: '#FFD700'
-        }, {
-          id: 1,
-          text: 'Text 02',
-          color: '#20B2AA'
-        }, {
-          id: 2,
-          text: 'Text 03',
-          color: '#90EE90'
-        }, {
-          id: 3,
-          text: 'Text 04',
-          color: '#87CEFA'
-        }, {
-          id: 4,
-          text: 'Text 05',
-          color: '#FFB6C1'
-        }, {
-          id: 5,
-          text: 'Text 06',
-          color: '#5F9EA0'
-        }, {
-          id: 6,
-          text: 'Text 07',
-          color: '#FFA07A'
-        }, {
-          id: 7,
-          text: 'Text 08',
-          color: '#00FA9A'
-        }
+        // {
+        //   id: 0,
+        //   text: 'Text 01',
+        //   color: '#FFD700'
+        // }, {
+        //   id: 1,
+        //   text: 'Text 02',
+        //   color: '#20B2AA'
+        // }, {
+        //   id: 2,
+        //   text: 'Text 03',
+        //   color: '#90EE90'
+        // }, {
+        //   id: 3,
+        //   text: 'Text 04',
+        //   color: '#87CEFA'
+        // }, {
+        //   id: 4,
+        //   text: 'Text 05',
+        //   color: '#FFB6C1'
+        // }, {
+        //   id: 5,
+        //   text: 'Text 06',
+        //   color: '#5F9EA0'
+        // }, {
+        //   id: 6,
+        //   text: 'Text 07',
+        //   color: '#FFA07A'
+        // }, {
+        //   id: 7,
+        //   text: 'Text 08',
+        //   color: '#00FA9A'
+        // }
       ]
     };
+  },
+  componentDidMount: function() {
+    var savedNotes = JSON.parse(localStorage.getItem('notes'));
+    if (savedNotes) {
+      this.setState({
+        notes: savedNotes
+      });
+    }
+  },
+  onNoteAdd: function(newNote) {
+    var newNotes = this.state.notes.slice();
+    newNotes.unshift(newNote);
+    this.setState({
+      notes: newNotes
+    }, this._updateLocalStorage);
   },
   render: function() {
       return (
         <div className="notes-app">
           Notes App
-          <NoteEditor />
+          <NoteEditor onNoteAdd={this.onNoteAdd} />
           <NotesGrid notes={this.state.notes} />
         </div>
       );
+    },
+    componentWillUnmount: function () {
+      this._updateLocalStorage();
+    },
+    _updateLocalStorage: function() {
+      var notes = JSON.stringify(this.state.notes);
+      localStorage.setItem('notes', notes);
     }
 });
 
 ReactDOM.render(
   <App />,
-  document.getElementById('content')
+  document.getElementById('mount-point')
 );
