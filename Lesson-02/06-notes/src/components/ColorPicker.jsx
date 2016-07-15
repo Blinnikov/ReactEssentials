6e7915918d5af1158f4ce99bf3d01aeb55c4677d
Igor Blinnikov <igor.blinnikov@gmail.com>
2016-07-15 22:36:20 +0300

    feat(notes): note color

--- Lesson-02/06-notes/src/components/ColorPicker.jsx
+++ Lesson-02/06-notes/src/components/ColorPicker.jsx
@@ -3,16 +3,39 @@ var React = require('react');
 require('./ColorPicker.less')
 
 var ColorPicker = React.createClass({
+  getInitialState: function() {
+    return {
+      currentColor: this.props.currentColor
+    }
+  },
+  setColor: function(color) {
+    var currentColor = this.state.currentColor === color ? '' : color;
+    this.setState({
+      currentColor: currentColor
+    });
+    this.props.onColorChanged(currentColor);
+  },
   render: function() {
+    var that = this;
+    var check = '\u2713';
+    var nbsp = '\u00a0';
     return (
       <div>
-        <button className="color-button red">&nbsp;</button>
-        <button className="color-button yellow" >&nbsp;</button>
-        <button className="color-button light-yellow" >&nbsp;</button>
-        <button className="color-button purple" >&nbsp;</button>
-        <button className="color-button blue" >&nbsp;</button>
-        <button className="color-button sky-blue" >&nbsp;</button>
-        <button className="color-button green" >&nbsp;</button>
+        {
+          this.props.colors.map(function(color) {
+            var style = { backgroundColor: color }
+            // return <button key={color} className="color-button" style={style}>&nbsp;</button>
+            var buttonText = that.state.currentColor === color ? check : nbsp;
+            return <button
+              key={color}
+              className="color-button"
+              style={style}
+              onClick={that.setColor.bind(null, color)} >
+                {buttonText}
+              </button>
+
+          })
+        }
       </div>
     );
   }
--- Lesson-02/06-notes/src/components/NoteEditor.jsx
+++ Lesson-02/06-notes/src/components/NoteEditor.jsx
@@ -5,16 +5,23 @@ require('./NoteEditor.less')
 
 var NoteEditor = React.createClass({
     getInitialState: function() {
-        return {text: ''}
+        return {
+          text: '',
+          currentColor: '#FFFF85',
+          colors: ['#FF897D', '#FFD27A', '#FFFF85', '#CFD8DC', '#7CD7FF', '#A4FFEB', '#CBFF8A']
+        }
     },
     onTextChange: function(event) {
         this.setState({text: event.target.value});
+    },
+    onColorChange: function(color) {
+      this.setState({ currentColor: color })
     },
     addNote: function() {
         var note = {
             id: Date.now(),
             text: this.state.text,
-            color: 'yellow'
+            color: this.state.currentColor
         };
         this.props.onNoteAdd(note);
         this.setState({text: ''});
@@ -24,12 +31,12 @@ var NoteEditor = React.createClass({
             <div className="note-editor">
                 <textarea placeholder="Enter yout text here.." rows={5} className="textarea" value={this.state.text} onChange={this.onTextChange}/>
                 <div className="groups">
-                  <div className="left-group">
-                    <ColorPicker />
-                  </div>
-                  <div className="right-group">
-                    <button className="add-button" onClick={this.addNote} disabled={!this.state.text} >Add</button>
-                  </div>
+                    <div className="left-group">
+                        <ColorPicker colors={this.state.colors} currentColor={this.state.currentColor} onColorChanged={this.onColorChange} />
+                    </div>
+                    <div className="right-group">
+                        <button className="add-button" onClick={this.addNote} disabled={!this.state.text}>Add</button>
+                    </div>
                 </div>
             </div>
         );
